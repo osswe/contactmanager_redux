ad2bb8fe6b1fb346fb4854c4080c87b3ddc73096
mmccue <mmccue@paylocity.com>
2018-08-19 01:13:05 -0500

    getting started on EditContact functionality

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -5,9 +5,17 @@ export const getContacts = () => async dispatch => {
   const res = await axios.get('https://jsonplaceholder.typicode.com/users');
   dispatch({ type: actions.GET_CONTACTS, payload: res.data });
 }
-export const addContact = (contact) => {
-  return { type: actions.ADD_CONTACT, payload: contact };
+
+export const addContact = (contact) => async dispatch => {
+  const res = await axios.post('https://jsonplaceholder.typicode.com/users', contact);
+  dispatch({ type: actions.ADD_CONTACT, payload: res.data });
 }
-export const deleteContact = (id) => {
-  return { type: actions.DELETE_CONTACT, payload: id };
+
+export const deleteContact = (id) => async dispatch => {
+  try {
+    await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+  } catch (exception) {
+    console.log("exception:", exception);
+  }
+  await dispatch({ type: actions.DELETE_CONTACT, payload: id });
 }
